2dd553b0c90379ebdcb11e5a505fb8fa10ad42bd
Iris <irisshing1211@gmail.com>
2019-11-13 17:49:08 +0800

    init

---
+++ EventSourcing1/Person.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+namespace EventSourcing1
+{
+    public class Person
+    {
+        private int age;
+        private EventBroker broker;
+
+        public Person(EventBroker b)
+        {
+            broker = b;
+            broker.Commands += BrokerOnCommands;
+            broker.Queries += BrokerOnQueries;
+        }
+
+        private void BrokerOnCommands(object sender, Command command)
+        {
+            var cac = command as ChangeAgeCommand;
+            if (cac != null && cac.Target == this)
+            {
+                if (cac.Reg) broker.AllEvents.Add(new AgeChangeEvent(this, age, cac.Age));
+                age = cac.Age;
+            }
+        }
+
+        private void BrokerOnQueries(object sender, Query query)
+        {
+            var ac = query as AgeQuery;
+            if (ac != null && ac.Target == this)
+            {
+                ac.Result = age;
+            }
+        }
+    }
+
+    public class EventBroker
+    {
+        // all event that happened
+        public IList<Event> AllEvents = new List<Event>();
+
+        // command, to do some action
+        public event EventHandler<Command> Commands;
+
+        // query
+        public event EventHandler<Query> Queries;
+
+        public void Command(Command c)
+        {
+            Commands?.Invoke(this, c);
+        }
+
+        public T Query<T>(Query q)
+        {
+            Queries?.Invoke(this, q);
+            return (T) q.Result;
+        }
+
+        public void UndoLast()
+        {
+            var e = AllEvents.LastOrDefault();
+            var ac = e as AgeChangeEvent;
+            if (ac != null)
+            {
+                Command(new ChangeAgeCommand(ac.Target, ac.Old) {Reg = false});
+                AllEvents.Remove(e);
+            }
+        }
+    }
+
+    public class Event
+    {
+        // backtrack
+    }
+
+    public class AgeChangeEvent : Event
+    {
+        public Person Target;
+        public int Old, New;
+
+        public AgeChangeEvent(Person p, int old, int newVal)
+        {
+            Target = p;
+            Old = old;
+            New = newVal;
+        }
+
+        public override string ToString()
+        {
+            return $"Age change from {Old} to {New}";
+        }
+    }
+
+    public class Command : EventArgs
+    {
+        public bool Reg = true;
+    }
+
+    public class ChangeAgeCommand : Command
+    {
+        public Person Target;
+        public int Age;
+
+        public ChangeAgeCommand(Person t, int age)
+        {
+            Target = t;
+            Age = age;
+        }
+    }
+
+    public class Query
+    {
+        public object Result;
+    }
+
+    public class AgeQuery : Query
+    {
+        public Person Target;
+    }
+}
---
+++ EventSourcing1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EventSourcing1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var eb = new EventBroker();
+            var p = new Person(eb);
+
+            eb.Command(new ChangeAgeCommand(p, 1));
+            foreach (var e in eb.AllEvents)
+            {
+                Console.WriteLine(e);
+            }
+
+            int age;
+            age = eb.Query<int>(new AgeQuery {Target = p});
+
+            eb.UndoLast();
+            foreach (var e in eb.AllEvents)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
